bef5a150a2f9b56f1a21732311bd153f87defc01
Nicolas Saraceno <nico.saraceno92@gmail.com>
2022-04-30 14:52:15 -0300

    Add loading state to the provider
    This is to prevent to render the application without the global data

--- application/src/contexts/App.js
+++ application/src/contexts/App.js
@@ -6,6 +6,7 @@ export default AppContext;
 
 export function AppContextProvider({ children }) {
   const [investments, setInvestments] = useState([]);
+  const [loadingData, setLoadingData] = useState(true)
 
   useEffect(() => {
     // populate bond and stocks data
@@ -13,10 +14,12 @@ export function AppContextProvider({ children }) {
     .then(response => response.json())
     .then(data => {
       setInvestments(data);
+      setLoadingData(false);
     });
   }, []);
 
   return (
+    !loadingData &&
     <AppContext.Provider
       value={{
         investments
