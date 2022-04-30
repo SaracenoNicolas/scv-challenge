1ed24a072afd6239583ee7f39a4fde8c6dbf2752
Nicolas Saraceno <nico.saraceno92@gmail.com>
2022-04-30 10:32:45 -0300

    Update BE Funcionality and Model
    I changed my mind about having the bonds and stocks separate, I don't see any difference in term on how the application manage them, so I won't make my life difficult here.
    I did added a type to know that there is a difference cause I think it would help later.
    
    I also moved some logic for the user investment list to the server cause this is that I would expect from a real API

--- server/index.js
+++ server/index.js
@@ -3,82 +3,91 @@ const express = require("express");
 const PORT = process.env.PORT || 3001;
 
 const app = express();
-
-let stockValues = [
+// Application data, this one should be persisted in a database
+let investments_data = [
   {
     id: 1,
     name: "Coca Cola",
+    type: "STOCK",
     value: 0.5,
   },
   {
     id: 2,
     name: "Apple",
+    type: "STOCK",
     value: 1.0
   },
   {
     id: 3,
     name: "Microsoft",
+    type: "STOCK",
     value: 1.5
   },
   {
     id: 4,
     name: "Google",
+    type: "STOCK",
     value: 2.0
-  }
-]
-
-let bondValues = [
+  },
   {
-    id: 1,
+    id: 5,
     name: "Bono A23",
+    type: "BOND",
     value: 200,
   },
   {
-    id: 2,
+    id: 6,
     name: "Bono E45",
+    type: "BOND",
     value: 540,
   },
   {
-    id: 3,
+    id: 7,
     name: "Bono F54",
+    type: "BOND",
     value: 432,
   },
   {
-    id: 4,
+    id: 8,
     name: "Bono T30",
+    type: "BOND",
     value: 111,
   },
+
 ]
 
 let userData = {
-  "stocks": [
+  "investments": [
     {
       "id": 1,
       "holdings": 200,
-    }
-  ],
-  "bonds": [
+    },
     {
-      "id": 1,
+      "id": 5,
       "holdings": 3000,
     }
   ],
   "cash": 5000,
 }
 
+// End of Application data
+
 // Return the available stocks
-app.get("/api/stocks", (req, res) => {
-  res.json(stockValues);
+app.get("/api/investments", (req, res) => {
+  res.json(investments_data);
 });
 
-// Return the available bonds
-app.get("/api/bonds", (req, res) => {
-  res.json(bondValues);
-});
-
-// Return the user holdings
+// (there is no user but this should be by user ID)
 app.get("/api/holdings", (req, res) => {
-  res.json(userData);
+  let responseJson = userData;
+
+  responseJson.investments.forEach((invesment, idx) => {
+    let investmentData = investments_data.find(investment => investment.id === invesment.id);
+    responseJson.investments[idx].name = investmentData.name;
+    responseJson.investments[idx].type = investmentData.type;
+  })
+
+  res.json(responseJson);
 });
 
 app.listen(PORT, () => {
